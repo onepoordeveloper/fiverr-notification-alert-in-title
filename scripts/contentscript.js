388a8bdadb30fc2773c51dc95208d65ca858dfcd
AhmadHassanAwan <ahmad.pgu1@hotmail.com>
2016-11-15 15:41:37 +0500

    icons updated.. basic functionality completed.. manifest updated

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+setInterval(function(){
+    checkForNotification();
+},5000);
+
+var checkForNotification = function(){
+    var redFav = chrome.extension.getURL("images/fiverr-red.png");
+    var oldFav = "https://www.fiverr.com/favicon.ico";
+    if ($("li[data-gtm-label='inbox']").hasClass("marked")){
+        $("link[rel='shortcut icon']").attr("href", redFav);
+    }
+    else{
+        $("link[rel='shortcut icon']").attr("href", oldFav);
+    }
+};
