64acb71e95ab4ef6a944e957791a5622fa417fac
Ahmad <onepoordeveloper@gmail.com>
2023-02-12 19:51:51 +0500

    update icon selector in dom v1.3.5

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -7,7 +7,7 @@ setInterval(function () {
 var checkForNotification = function () {
     var redFav = chrome.extension.getURL("images/fiverr-red.png");
     var oldFav = "https://www.fiverr.com/favicon.ico";
-    if ($(".unread").length > 0) {
+    if ($(".unread-icon").length > 0) {
         $("link[rel*='icon']").attr("href", redFav);
     }
     else {
